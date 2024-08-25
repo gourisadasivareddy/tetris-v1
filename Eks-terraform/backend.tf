fe87bc12d6c5cb48e2b63c313685531312208540
gourisadasivareddy <81284608+gourisadasivareddy@users.noreply.github.com>
2024-08-25 02:05:27 +0100

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "siva-project" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "Eks/terraform.tfstate"
     region = "eu-north-1"
   }
 }
